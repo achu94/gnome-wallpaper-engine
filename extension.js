4508c51693c218b2aae5e199bfa395acde6552b8
Aygyun <aygyun_94@outlook.de>
2026-03-27 12:52:12 +0100

    perf: replace mpv with ffmpeg for thumbnail generation
    Switch thumbnail generation from mpv to ffmpeg to resolve performance issues.
    mpv caused slowdowns and higher resource usage, while ffmpeg provides faster
    and more efficient thumbnail extraction.

--- extension.js
+++ extension.js
@@ -9,9 +9,21 @@ import { AutoPause } from "./modules/autoPause.js";
 export default class WallpaperExtension extends Extension {
     enable() {
         this._mpvExists = GLib.find_program_in_path('mpv');
-
+        this._ffmpegExists = GLib.find_program_in_path('ffmpeg');
+        
+        let missing = [];
+        
         if (!this._mpvExists) {
-            Main.notify("Gnome Wallpaper Engine", "ERROR: 'mpv' not found!");
+            missing.push("'mpv'");
+        }
+        
+        if (!this._ffmpegExists) {
+            missing.push("'ffmpeg'");
+        }
+        
+        if (missing.length > 0) {
+            let msg = "Error: Missing dependencies: " + missing.join(" and ");
+            Main.notify("Gnome Live Wallpaper", msg);
             return;
         }
 
