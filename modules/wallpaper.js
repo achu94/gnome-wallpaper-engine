a688bce6afa34fb4c1c5ae29bccb51a6b1b648fc
Aygyun <aygyun_94@outlook.de>
2026-03-29 20:45:03 +0200

    wip: implementing Clutter

--- modules/wallpaper.js
+++ modules/wallpaper.js
@@ -1,7 +1,11 @@
 import Gio from "gi://Gio";
 import GLib from "gi://GLib";
+import * as Main from "resource:///org/gnome/shell/ui/main.js";
+
+import Clutter from "gi://Clutter";
 
 import { WindowUtils } from "./windowUtils.js";
+import { debug } from "./utils.js";
 
 export class Wallpaper {
     constructor(ext) {
@@ -16,6 +20,36 @@ export class Wallpaper {
         this._lowerFixApplied = false;
     }
 
+    clone(actor) {
+        try {
+            const clutterClone = new Clutter.Clone({
+                source: actor,
+            });
+
+            debug("1: cloned");
+
+            // 👉 FULLSCREEN
+            let [width, height] = global.display.get_size();
+            clutterClone.set_position(0, 0);
+            clutterClone.set_size(width, height);
+
+            debug("2: fullscreen");
+
+            // 👉 IN BACKGROUND (WICHTIG!)
+            Main.layoutManager._backgroundGroup.add_child(clutterClone);
+            debug("3: in background");
+
+            // 👉 echtes Fenster verstecken
+            actor.opacity = 0;
+
+            debug("4: main mpv hidden");
+
+            return clutterClone;
+        } catch (error) {
+            debug(error);
+        }
+    }
+
     start() {
         this.stop();
 
@@ -23,7 +57,11 @@ export class Wallpaper {
         const filename = settings.get_string("current-wallpaper");
         if (!filename) return;
 
-        const videoPath = GLib.build_filenamev([this._ext.path, "backgrounds", filename]);
+        const videoPath = GLib.build_filenamev([
+            this._ext.path,
+            "backgrounds",
+            filename,
+        ]);
 
         const cmd = [
             "mpv",
@@ -48,7 +86,10 @@ export class Wallpaper {
         ];
 
         try {
-            this._mpvProcess = Gio.Subprocess.new(cmd, Gio.SubprocessFlags.NONE);
+            this._mpvProcess = Gio.Subprocess.new(
+                cmd,
+                Gio.SubprocessFlags.NONE,
+            );
 
             let attempts = 0;
 
@@ -72,9 +113,8 @@ export class Wallpaper {
             this._findWindowTimeoutId = GLib.timeout_add(
                 GLib.PRIORITY_DEFAULT,
                 150,
-                findWindow
+                findWindow,
             );
-
         } catch (e) {
             logError(e);
         }
@@ -87,13 +127,20 @@ export class Wallpaper {
             const metaWin = actor.get_meta_window();
 
             if (WindowUtils.isWallpaperWindow(metaWin)) {
+                this.clone(actor);
+                GLib.idle_add(GLib.PRIORITY_DEFAULT, () => {
+                    return GLib.SOURCE_REMOVE;
+                });
+
+                return;
+
                 metaWin.lower();
                 metaWin.stick();
                 metaWin.focus_on_click = false;
 
                 try {
                     metaWin.set_accept_focus(false);
-                } catch (_) { }
+                } catch (_) {}
 
                 if (!this._lowerFixApplied) {
                     this._lowerFixApplied = true;
@@ -109,22 +156,25 @@ export class Wallpaper {
                 GLib.idle_add(GLib.PRIORITY_DEFAULT_IDLE, () => {
                     try {
                         metaWin.set_input_region(null);
-                    } catch (_) { }
+                    } catch (_) {}
                     return GLib.SOURCE_REMOVE;
                 });
 
                 if (!this._wallpaperWindow) {
                     this._wallpaperWindow = metaWin;
 
-                    this._raisedSignalId = metaWin.connect('raised', () => {
+                    this._raisedSignalId = metaWin.connect("raised", () => {
                         metaWin.lower();
                     });
 
-                    this._windowCreatedId = global.display.connect('window-created', () => {
-                        if (this._wallpaperWindow) {
-                            this._wallpaperWindow.lower();
-                        }
-                    });
+                    this._windowCreatedId = global.display.connect(
+                        "window-created",
+                        () => {
+                            if (this._wallpaperWindow) {
+                                this._wallpaperWindow.lower();
+                            }
+                        },
+                    );
                 }
 
                 return true;
